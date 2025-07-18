f26cbccc67de8b4500e137b4b1e9a67b35c31cba
TempUser <temp@mail>
2025-07-18 17:36:18 +0300

    Better debug

--- src/agzam4/debug/Debug.java
+++ src/agzam4/debug/Debug.java
@@ -1,16 +1,26 @@
 package agzam4.debug;
 
-import arc.scene.Group;
+import agzam4.ModWork;
 import mindustry.Vars;
-import mindustry.content.Blocks;
-import mindustry.content.Items;
-import mindustry.game.Team;
-import mindustry.gen.Call;
-import mindustry.gen.Groups;
-import mindustry.world.blocks.storage.CoreBlock.CoreBuild;
 
 public class Debug {
 
+	public static boolean debug = ModWork.settingDef("debug", false);
+
+	public static void enable() {
+		debug = true;
+		ModWork.setting("debug", true);
+	}
+
+	public static void disable() {
+		debug = false;
+		ModWork.setting("debug", false);
+	}
+	
+	
+	public static void info(Object object) {
+		new ObjectInspector(object).show();
+	}
 	
 	public static void init() {
 		Vars.mods.getScripts().context.evaluateString(Vars.mods.getScripts().scope,
@@ -37,6 +47,126 @@ public class Debug {
 //		Call.setRules(Vars.state.rules);
 //		Team.crux.cores().each(c -> c.items.add(Items.fissileMatter, 99999));
 //		Vars.state.rules.bannedBlocks.add(Blocks.scorch, Blocks.lancer, Blocks.arc, Blocks.parallax, Blocks.fuse, Blocks.meltdown)
+//		Events.on(mindustry.game.EventType.ModContentLoadEvent.class, e -> {
+//			exportColors();
+//		});
+	}
+	
+	// AgzamDebug.exportColors()
+
+//	public static void exportColors() {
+//		int res = 3;
+//		int size = 0;
+//		
+//		for (Block b : Vars.content.blocks()) {
+//			size += b.size*b.size;
+//		}
+////		BufferedImage pal = new BufferedImage(size, res*res, BufferedImage.TYPE_INT_ARGB);
+//		Pixmap pal = new Pixmap(size, res*res);
+//		int index = 0;
+////		Log.info("pal size: @", pal);
+//		ObjectMap<Pixmap, Pixmap> maps = new ObjectMap<Pixmap, Pixmap>();
+//
+//		for (Block b : Vars.content.blocks()) {
+//			TextureRegion region = b.uiIcon;
+//			if(region == null) {
+//				Log.info("region is null: @", b);
+//				return;
+//			}
+//			PixmapRegion t = Core.atlas.getPixmap(region);
+//			if(!maps.containsKey(t.pixmap)) {
+//				PixmapIO.writePng(Fi.get("debug/#pixmap_" + maps.size + ".png"), t.pixmap);
+//				maps.put(t.pixmap, t.pixmap);
+//			}
+////			region.texture.p
+//			t.get(index, index);
+//			int rx = t.x;
+//			int ry = t.y;
+//			int rw = t.width;
+//			int rh = t.height;
+//			Log.info("@ region @;@ @x@", b.name, rx,ry,rw,rh);
+//			
+//			int rsize = res*b.size;
+//			Pixmap debug = new Pixmap(rsize, rsize);
+//			
+//			for (int dy = 0; dy < rsize; dy++) {
+//				int y1 = dy*t.height/rsize;
+//				int y2 = (dy+1)*t.height/rsize;
+//				for (int dx = 0; dx < rsize; dx++) {
+//					int x1 = dx*t.width/rsize;
+//					int x2 = (dx+1)*t.width/rsize;
+//					
+//					ObjectMap<Integer, Counter> colors = new ObjectMap<Integer, Debug.Counter>();
+////					Log.info("@ sub: @->@ @->@", b.name, x1,x2,y1,y2);
+//					
+//					float cr = 0, cg = 0, cb = 0, ca = 0;
+//					float count = 0;
+//
+//					for (int py = y1; py < y2; py++) {
+//						for (int px = x1; px < x2; px++) {
+//							Color c = new Color(t.getRaw(px, py));
+//							cr += c.r;
+//							cg += c.g;
+//							cb += c.b;
+//							ca += c.a;
+//							count++;
+//						}
+//					}
+//
+//					cr /= count;
+//					cg /= count;
+//					cb /= count;
+//					ca /= count;
+//					
+//					/*
+//					for (int py = y1; py < y2; py++) {
+//						for (int px = x1; px < x2; px++) {
+//							int rgba = t.getRaw(px, py);
+//							Counter counter = colors.get(rgba);
+//							if(counter == null) {
+//								counter = new Counter();
+//								colors.put(rgba, counter);
+//							}
+//							counter.inc();
+//						}
+//					}
+//					int rgba = Color.red.rgba8888();
+//					int count = 0;
+//					for (int color : colors.keys()) {
+//						Counter counter = colors.get(color);
+//						if(counter.count < count) continue;
+//						count = counter.count;
+//						rgba = color;
+//					}
+//					//*/
+//					
+////					Log.info(new Color(rgba));
+//					
+//					int rgba = Color.rgba8888(cr, cg, cb, ca);
+//					int ind = index++;
+//					
+//					debug.setRaw(dx, dy, rgba); // t.getRaw(dx*t.width/rsize, dy*t.height/rsize)
+//					pal.setRaw(ind/9, ind%9, rgba);
+//				}
+//			}
+//			PixmapIO.writePng(Fi.get("debug/" + b.name + ".png"), debug);
+//			debug.dispose();
+//			
+//		}
+//		
+////		Log.info("saving");
+//		PixmapIO.writePng(Fi.get("pal.png"), pal);
+//		
+//	}
+	
+	private static class Counter {
+		
+		int count = 0;
+		
+		public void inc() {
+			count++;
+		}
+		
 	}
 	
 }
---
+++ src/agzam4/debug/ObjectInspector.java
@@ -0,0 +1,162 @@
+package agzam4.debug;
+
+import static arc.Core.app;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import arc.audio.Sound;
+import arc.func.Cons2;
+import arc.graphics.Color;
+import arc.graphics.g2d.TextureRegion;
+import arc.scene.style.TextureRegionDrawable;
+import arc.scene.ui.ScrollPane;
+import arc.scene.ui.TextField;
+import arc.scene.ui.layout.Table;
+import arc.util.Align;
+import mindustry.entities.Effect;
+import mindustry.gen.Icon;
+import mindustry.graphics.Pal;
+import mindustry.ui.Styles;
+import mindustry.ui.dialogs.BaseDialog;
+
+public class ObjectInspector extends BaseDialog {
+	
+	private Object object;
+    private String searchText = "";
+    
+    protected Table mainTable;
+
+	public ObjectInspector(Object object) {
+		super("");
+		this.object = object;
+		title.setColor(Color.white);
+		titleTable.remove();
+		closeOnBack();
+		
+		mainTable = new Table();
+        ScrollPane pane = new ScrollPane(mainTable);
+        pane.setFadeScrollBars(false);
+
+        cont.table(table -> {
+            table.left();
+            table.image(Icon.zoom);
+            TextField field = table.field(searchText, res -> {
+                searchText = res;
+                build();
+            }).growX().get();
+
+            shown(() -> {
+                field.setText(searchText = "");
+                build();
+                app.post(field::requestKeyboard);
+            });
+        }).fillX().padBottom(4).top();
+
+        cont.row();
+        cont.add(pane).grow();
+        
+		build();
+	}
+	
+	private void build() {
+		Table t = mainTable;
+		
+        t.clear();
+        t.add().height(10);
+        t.row();
+
+        t.add("Object").color(Pal.accent).colspan(4).pad(10).padBottom(4).get().setAlignment(Align.left);
+        t.row();
+        t.image().color(Pal.accent).fillX().height(3).pad(6).colspan(4).padTop(0).padBottom(10).row();
+        t.labelWrap("Object: " + object).growX().wrapLabel(false).row();
+
+        if(object == null) return;
+
+        t.labelWrap("Class: " + object.getClass()).growX().wrapLabel(false).row();
+
+
+        t.add("Declared Fields").color(Pal.accent).colspan(4).pad(10).padBottom(4).get().setAlignment(Align.left);
+        t.row();
+        t.image().color(Pal.accent).fillX().height(3).pad(6).colspan(4).padTop(0).padBottom(10).row();
+
+        Cons2<Field[], Object> displayFields = (fields, src) -> {
+        	Arrays.sort(fields, (f1,f2) -> f1.getName().compareTo(f2.getName()));
+        	for (Field field : fields) {
+        		//							if(field.getType() == TextureRegion.class) continue;
+        		if(field.getType() == Sound.class) continue;
+        		if(field.getType() == Effect.class) continue;
+        		if(!field.getName().contains(searchText)) continue;
+        		try {
+        			field.setAccessible(true);
+        			Object obj = Modifier.isStatic(field.getModifiers()) ? field.get(null) : field.get(src);
+        			if(obj == null) {
+        				t.labelWrap("[white]" + field.getName() + ": [magenta]null").wrapLabel(false).growX().row();
+        				continue;
+        			}
+        			String str = "";
+        			str = "[lightgray]" + obj;
+        			if(obj instanceof Boolean) str = ((Boolean) obj) ? "[lime]true" : "[red]false";
+        			if(obj instanceof TextureRegion) {
+        				TextureRegion reg = (TextureRegion) obj;
+        				t.button(" " + field.getName(), new TextureRegionDrawable(reg), Styles.grayt, 25f, () -> {
+        					new ObjectInspector(obj).show();
+        				}).align(Align.left).wrapLabel(false).growX().get().getLabel().setAlignment(Align.left);
+        				t.row();
+        				continue;
+        			}
+        			if(obj instanceof Sound) continue;
+        			if(obj instanceof Color) str = "[#" + obj.toString() + "]" + obj;
+        			t.button(field.getName() + ": " + str, Styles.grayt, () -> {
+    					new ObjectInspector(obj).show();
+        			}).align(Align.left).wrapLabel(false).growX().get().getLabel().setAlignment(Align.left);
+        			t.row();
+        		} catch (Error | Exception e) {
+//    				t.labelWrap("[white]" + field.getName() + ": [red]" + e.getMessage()).wrapLabel(false).growX().row();
+        			continue;
+        		}
+        	}
+        };
+        
+        displayFields.get(object.getClass().getDeclaredFields(), object);
+
+        t.add("Fields").color(Pal.accent).colspan(4).pad(10).padBottom(4).get().setAlignment(Align.left);
+        t.row();
+        t.image().color(Pal.accent).fillX().height(3).pad(6).colspan(4).padTop(0).padBottom(10).row();
+
+        displayFields.get(object.getClass().getFields(), object);
+
+        t.add("Methods").color(Pal.accent).colspan(4).pad(10).padBottom(4).get().setAlignment(Align.left);
+        t.row();
+        t.image().color(Pal.accent).fillX().height(3).pad(6).colspan(4).padTop(0).padBottom(10).row();
+
+        Cons2<Method[], Object> displayMethods = (methods, src) -> {
+        	Arrays.sort(methods, (f1,f2) -> f1.getName().compareTo(f2.getName()));
+        	for (Method method : methods) {
+        		if(!method.getName().contains(searchText)) continue;
+        		try {
+        			method.setAccessible(true);
+        			if(method.getParameters().length == 0) {
+        				t.button(method.getName(), Styles.grayt, () -> {
+        					try {
+            					new ObjectInspector(method.invoke(object)).show();
+        					} catch (Error | Exception e) {
+        						e.printStackTrace();
+        					}
+        				}).align(Align.left).growX().wrapLabel(false).get().getLabel().setAlignment(Align.left);
+        				t.row();
+        				continue;
+        			}
+        			t.labelWrap(method.getName()).growX().wrapLabel(false);
+        			t.row();
+        		} catch (Error | Exception e) {
+        			continue;
+        		}
+        	}
+        };
+
+        displayMethods.get(object.getClass().getMethods(), object);
+	}
+	
+}
