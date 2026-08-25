3d738bbecb95c2eb45aac2003888be43a5e97f60
Agzam4 <75160721+Agzam4@users.noreply.github.com>
2026-08-25 03:59:42 +0300

    feat: dev util for blocks classes list

---
+++ src/agzam4/dev/ListClasses.java
@@ -0,0 +1,36 @@
+package agzam4.dev;
+
+import arc.struct.*;
+import arc.util.Log;
+import mindustry.Vars;
+import mindustry.content.*;
+import mindustry.core.ContentLoader;
+
+public class ListClasses {
+
+	
+	public static void main(String[] args) {
+		Vars.content = new ContentLoader();
+		Liquids.load();
+		Items.load();
+		Blocks.load();
+
+		ObjectSet<Class<?>> classes = ObjectSet.with();
+		ObjectSet<Class<?>> interfaces = ObjectSet.with();
+		
+		Vars.content.blocks().forEach(b -> {
+			Class<?> cls = b.getClass();
+			while (true) {
+				if(!cls.toString().contains("$")) classes.add(cls);
+				interfaces.addAll(cls.getInterfaces());
+				cls = cls.getSuperclass();
+				if(cls == Object.class) return;
+			}
+		});
+
+		Log.info("=== classes ===\n@", classes.toSeq().sort((s1,s2)->s1.toString().compareTo(s2.toString())).toString("\n"));
+		Log.info("=== interfaces ===\n@", interfaces.toSeq().toString("\n"));
+		
+	}
+	
+}
